59915e71ba8790a400ed33a2148f1c550b5dc2a5
Paul Miranda <paul.miranda@uabc.edu.mx>
2019-04-03 13:02:40 -0700

    view posts, edit and delete posts functionality added

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ app_name = 'blog'
 urlpatterns = [
     path('',views.PostListView.as_view(),name='post_list'),
     path('article/', views.ArticleView.as_view(), name="article"),
-    path('newpost/', views.CreatPostView.as_view(), name="new_post")
+    path('newpost/', views.CreatPostView.as_view(), name="new_post"),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView, CreateView, ListView)
+from django.views.generic import (TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from blog.models import Post
 from blog.forms import PostForm
 from django.utils import timezone
+from django.urls import reverse_lazy
 
 # Create your views here.
 class ArticleView(TemplateView):
@@ -21,6 +22,25 @@ class PostListView(ListView):
     model = Post
     def get_queryset(self):
         return Post.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView, LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+
+    model = Post
+
+class PostDeleteView(DeleteView, LoginRequiredMixin):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
+
+
+    
 
 
 
